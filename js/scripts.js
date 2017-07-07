8abf57e0503ff77f79a3cec0a55a8a5f85354ebe
Charlie Kelson and Kim Schulze <kim@>
2017-07-06 17:56:58 -0700

    Uses conditional statements and functions to verify that triangle is indeed a triangle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,41 @@
-var largestNumber = function(side1, side2, side3) {
-  if (side1 > side2) {
+var largestSide = function(side1, side2, side3) {
+  if (side1 > side2 && side1 > side3) {
     return side1;
   }
-  else if (side2 > side3){
+  else if (side2 > side3) {
     return side2;
   }
   else {
     return side3;
   }
-}
+};
 
-var twoSmallerSides = function(largestNumber) {
-  
-}
+var twoSmallerSides = function(largestSide, side1, side2, side3) {
+  if (largestSide === side1) {
+    return side2 + side3;
+  }
+  else if (largestSide === side2) {
+    return side1 + side3;
+  }
+  else {
+    return side1 + side2;
+  }
+};
 
 $(function() {
   $("#triangle-tracker").submit(function(event) {
     event.preventDefault();
-    var side1 = $("#side1").val();
-    var side2 = $("#side2").val();
-    var side3 = $("#side3").val();
+    var side1 = parseFloat($("#side1").val());
+    var side2 = parseFloat($("#side2").val());
+    var side3 = parseFloat($("#side3").val());
 
-    side1 = parseFloat(side1);
-    side2 = parseFloat(side2);
-    side3 = parseFloat(side3);
+    var oneSide = largestSide(side1, side2, side3);
+    var twoSides = twoSmallerSides(oneSide, side1, side2, side3);
 
-    if () {
+    console.log(oneSide);
+    console.log(twoSides);
+
+    if (oneSide < twoSides) {
       if(side1 === side2 && side2 === side3) {
         $(".equilateral").show();
       }
@@ -38,7 +48,6 @@ $(function() {
     }
     else {
       $(".not-triangle").show();
-    }
     }
   });
 });
